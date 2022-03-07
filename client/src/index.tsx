d69879bbcdc0bd53123a41ea62455413c662f2ce
Ligalaiz <slavakuz88@gmail.com>
2022-03-07 22:58:39 +0300

    refactor: change apollo client option

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,6 +9,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql',
   cache: new InMemoryCache(),
+  connectToDevTools: true,
 });
 
 setTimeout(() =>
